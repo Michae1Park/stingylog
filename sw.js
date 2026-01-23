0874a7e98ffd8b983adb074fd5631700225b45c3
Michae1Park <parkmjb@gmail.com>
2026-01-23 18:01:26 +0900

    feat: add piggy icon, cleanup code

--- sw.js
+++ sw.js
@@ -1,19 +1,24 @@
+const cacheName = "StingyLog";
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json"
+]; 
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("expense-v1").then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./app.js",
-        "./manifest.json"
-      ])
-    )
+    caches.open(cacheName).then(cache => {
+        return cache.addAll(urlsToCache)
+    })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request).then((resp) => {
+        return resp || fetch(event.request)
+    })
   );
 });
