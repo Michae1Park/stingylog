03d47bac22eb3c662cc15445d1634c6d75939e85
Michae1Park <parkmjb@gmail.com>
2026-01-30 15:03:12 +0900

    docs: adding comments to pwa files

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
+// Name of the cache where assets will be stored
 const cacheName = "StingyLog";
+
+// List of files and routes to cache for offline use
 const urlsToCache = [
     "./",
     "./index.html",
@@ -7,17 +10,28 @@ const urlsToCache = [
     "./manifest.json"
 ]; 
 
+// Service worker install event
+// This runs when the service worker is first installed
 self.addEventListener("install", event => {
+  // waitUntil ensures the service worker
+  // doesn't finish installing until caching is complete
   event.waitUntil(
+    // Open (or create) the cache with the given name
     caches.open(cacheName).then(cache => {
+      // Add all specified files to the cache
         return cache.addAll(urlsToCache)
     })
   );
 });
 
+// Fetch event
+// This runs every time the app makes a network request
 self.addEventListener("fetch", event => {
   event.respondWith(
+    // Check if the requested resource exists in the cache
     caches.match(event.request).then((resp) => {
+        // If found in cache, return it
+        // Otherwise, fetch it from the network
         return resp || fetch(event.request)
     })
   );
